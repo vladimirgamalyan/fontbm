0f2b3347749188774598574b48e8afd5c4934906
Vladimir Gamalian <vladimir.gamalian@gmail.com>
2015-07-25 21:39:31 +0700

    "Hello, World!" for trigger travis.

--- src/main.cpp
+++ src/main.cpp
@@ -1,6 +1,7 @@
 #include <iostream>
 
 int main(int /*argc*/, char** /*argv*/) try {
+	std::cout << "Hello, World!" << srd::endl;
 	return 0;
 } catch (std::exception& e) {
 	std::cerr << "Error: " << e.what() << std::endl;
