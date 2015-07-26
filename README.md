59f847bf7d49dcfe4e81b4f3dd835afaa9db1d80
Vladimir Gamalian <vladimir.gamalian@gmail.com>
2015-07-26 11:36:35 +0700

    Try to render text to texture and write to file.

--- README.md
+++ README.md
@@ -2,3 +2,9 @@
 [![Build Status](https://travis-ci.org/vladimirgamalian/fontbm.svg)](https://travis-ci.org/vladimirgamalian/fontbm)
 
 Command line font generator, compatible with BMFont.
+
+
+Thanks too
+
+* [SDL_SavePNG](https://github.com/driedfruit/SDL_SavePNG)
+* [www.libpng.org](http://www.libpng.org/)
--- src/main.cpp
+++ src/main.cpp
@@ -1,7 +1,21 @@
 #include <iostream>
+#include <SDL2/SDL.h>
+#include <SDL2pp/SDL2pp.hh>
+#include "sdlSavePng/savepng.h"
 
 int main(int /*argc*/, char** /*argv*/) try {
-	std::cout << "Hello, World!" << std::endl;
+
+    SDL2pp::SDL sdl(SDL_INIT_VIDEO);
+    SDL2pp::Window window("fontbm", SDL_WINDOWPOS_UNDEFINED, SDL_WINDOWPOS_UNDEFINED, 640, 480, SDL_WINDOW_RESIZABLE);
+    SDL2pp::Renderer render(window, -1, SDL_RENDERER_ACCELERATED);
+
+    SDL2pp::SDLTTF ttf;
+    SDL2pp::Font font("./testdata/Vera.ttf", 32);
+
+    SDL2pp::Surface surface = font.RenderText_Blended("Hello, world!", SDL_Color {255, 255, 255, 255} );
+    std::cout << surface.GetSize() << std::endl;
+    SDL_SavePNG(surface.Get(), "./testdata/output.png");
+
 	return 0;
 } catch (std::exception& e) {
 	std::cerr << "Error: " << e.what() << std::endl;
