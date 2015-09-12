e66010d1e36fda9c895f373e724116cf755a8834
Vladimir Gamalian <vladimir.gamalian@gmail.com>
2015-09-12 21:24:03 +0700

    Fix build

--- src/example/Example.cpp
+++ src/example/Example.cpp
@@ -7,28 +7,33 @@
 
 using namespace SDL2pp;
 
-int main(int /*argc*/, char** /*argv*/) try {
-    SDL sdl(SDL_INIT_VIDEO);
-    Window window("SDL2pp demo",
-                  SDL_WINDOWPOS_UNDEFINED, SDL_WINDOWPOS_UNDEFINED,
-                  640, 480,
-                  SDL_WINDOW_RESIZABLE);
+int main(int /*argc*/, char** /*argv*/)
+{
+    try
+    {
+        SDL sdl(SDL_INIT_VIDEO);
+        Window window("SDL2pp demo",
+                      SDL_WINDOWPOS_UNDEFINED, SDL_WINDOWPOS_UNDEFINED,
+                      640, 480,
+                      SDL_WINDOW_RESIZABLE);
 
-    Renderer renderer(window, -1, SDL_RENDERER_ACCELERATED);
-    //Texture sprites(renderer, DATA_PATH "/M484SpaceSoldier.png");
-    renderer.Clear();
-    //renderer.Copy(sprites);
-    renderer.Present();
+        Renderer renderer(window, -1, SDL_RENDERER_ACCELERATED);
+        //Texture sprites(renderer, DATA_PATH "/M484SpaceSoldier.png");
+        renderer.Clear();
+        //renderer.Copy(sprites);
+        renderer.Present();
 
-    BmFont bmFont("vera.fnt");
+        BmFont bmFont("vera.fnt");
 
-    BmFontPrinter bmFontPrinter(renderer);
-    bmFontPrinter.print(SDL2pp::Point(0, 0), bmFont, "Hello, World!");
+        BmFontPrinter bmFontPrinter(renderer);
+        bmFontPrinter.print(SDL2pp::Point(0, 0), bmFont, "Hello, World!");
 
-    SDL_Delay(5000);
+        SDL_Delay(5000);
 
-    return 0;
-} catch (std::exception& e) {
-    std::cerr << e.what() << std::endl;
-    return 1;
+        return 0;
+    }
+    catch (std::exception& e) {
+        std::cerr << e.what() << std::endl;
+        return 1;
+    }
 }
