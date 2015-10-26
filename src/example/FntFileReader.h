82d9915be7481fb0e0a0f0eed8f01a95aa1efd57
Vladimir Gamalian <vladimir.gamalian@gmail.com>
2015-10-26 17:37:04 +0700

    Add fnt file reader class

---
+++ src/example/FntFileReader.h
@@ -0,0 +1,33 @@
+﻿#pragma once
+
+class FntFileReader
+{
+public:
+	void loadFromFile(const std::string& fileName);
+
+	int paddingTop;
+	int paddingRight;
+	int paddingBottom;
+	int paddingLeft;
+
+	struct Character
+	{
+		int x;
+		int y;
+		int w;
+		int h;
+		int xoffset;
+		int yoffset;
+		int xadvance;
+		int page;
+	};
+
+	std::map<uint32_t, Character> characters;
+	std::map<int, std::string> pages;
+
+private:
+	static std::string getValueString(const std::string& line, const std::string& key);
+	static std::string getQuotedValueString(const std::string& line, const std::string& key);
+	static int getValueInt(const std::string& line, const std::string& key, int default = -1);
+	static std::vector<int> getCsvInt(const std::string& str);
+};
