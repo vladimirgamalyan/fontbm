cb73b220f08dcad6d491039f61f1bd1d411685a5
Vladimir Gamalyan <vladimir.gamalian@gmail.com>
2018-02-09 13:34:20 +0300

    Simplify extractFileName function

--- src/utils/extractFileName.h
+++ src/utils/extractFileName.h
@@ -2,11 +2,10 @@
 
 #include <string>
 
-inline std::string extractFileName(const std::string& s)
+inline std::string extractFileName(std::string s)
 {
-    auto result(s);
     const auto slash = s.find_last_of("\\/");
     if (slash != std::string::npos)
-        result.erase(0, slash + 1);
-    return result;
+        s.erase(0, slash + 1);
+    return s;
 }
