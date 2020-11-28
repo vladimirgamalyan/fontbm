16688f92ee3f9d2ece7c663769a72d5a30b00d45
Vladimir Gamalyan <vladimir.gamalyan@gmail.com>
2020-11-28 14:59:47 -0800

    Update functional tests to Python 3

--- tests/functional_tests.py
+++ tests/functional_tests.py
@@ -33,39 +33,41 @@ def check_diff(expected, generated, binary=False):
 
 
 def main(argv):
-    assert len(argv) == 2
+    assert len(argv) == 3
     font_exe = argv[1]
+    runtime_lib_dir = argv[2]
     assert os.path.isfile(font_exe)
+    assert os.path.isdir(runtime_lib_dir)
 
     shutil.rmtree('generated', ignore_errors=True)
     os.makedirs('generated')
 
-    subprocess.check_call([font_exe, '--font-file', 'fonts/FreeSans.ttf', '--chars', '32-126',
+    my_env = os.environ.copy()
+    my_env['PATH'] = os.pathsep.join((runtime_lib_dir, my_env.get('PATH', '')))
+
+    subprocess.run([font_exe, '--font-file', 'fonts/FreeSans.ttf', '--chars', '32-126',
         '--padding-up', '8', '--padding-right', '7', '--padding-down', '6', '--padding-left', '5',
-        '--output', 'generated/test0', '--include-kerning-pairs'])
+        '--output', 'generated/test0', '--include-kerning-pairs'], check=True, env=my_env)
     check_diff('expected/test0.fnt', 'generated/test0.fnt')
 
-    subprocess.check_call([font_exe, '--font-file', 'fonts/FreeSans.ttf', '--chars', '32-126',
+    subprocess.run([font_exe, '--font-file', 'fonts/FreeSans.ttf', '--chars', '32-126',
         '--output', 'generated/test1', '--include-kerning-pairs',
         '--padding-up', '8', '--padding-right', '7',
-        '--data-format', 'xml'])
+        '--data-format', 'xml'], check=True, env=my_env)
     check_diff('expected/test1.fnt', 'generated/test1.fnt')
 
-    subprocess.check_call([font_exe, '--font-file', 'fonts/FreeSans.ttf', '--chars', '32-126',
+    subprocess.run([font_exe, '--font-file', 'fonts/FreeSans.ttf', '--chars', '32-126',
         '--output', 'generated/test2', '--include-kerning-pairs',
         '--spacing-vert', '4', '--spacing-horiz', '5',
-        '--data-format', 'bin'])
+        '--data-format', 'bin'], check=True, env=my_env)
     check_diff('expected/test2.fnt', 'generated/test2.fnt', True)
-
-    subprocess.check_call([font_exe, '--font-file', 'fonts/FreeSans.ttf', '--chars', '32-126',
-        '--output', 'generated/test3', '--include-kerning-pairs',
-        '--texture-width', '32', '--texture-height', '32'])
-    check_diff('expected/test3.fnt', 'generated/test3.fnt')
 
     process = subprocess.Popen([font_exe, '--font-file', 'fonts/FreeSans.ttf', '--chars', '32-126',
                                 '--output', 'generated/test3', '--include-kerning-pairs',
-                                '--texture-width', '32', '--texture-height', '32',
-                                '--max-texture-count', '4'], stdout=subprocess.PIPE, stderr=subprocess.PIPE)
+                                '--texture-width', '128', '--texture-height', '128',
+                                '--max-texture-count', '1'],
+                               stdout=subprocess.PIPE, stderr=subprocess.PIPE,
+                               env=my_env, text=True)
     out, err = process.communicate()
     assert out == ''
     assert err.strip() == 'too many generated textures (more than --max-texture-count)'
